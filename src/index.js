4f108634fa3a84a6d55cbbcb25902b16aedcd20b
StruzhakS <StruzhakS@gmail.com>
2023-05-10 00:32:50 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import axios from 'axios';
+const BASE_URL = 'https://pixabay.com/api/';
+const options = new URLSearchParams({
+  'API-key': '36236073-560374128ebdbbd6821cfa057',
+});
 
 const form = document.querySelector('.search-form');
+fetch(BASE_URL, options)
+  .then(res => res.json())
+  .then(console.log);
