7f6178ab67ebbb1941eb3a720ae823915262e0ef
StruzhakS <111219358+StruzhakS@users.noreply.github.com>
2023-05-09 21:33:25 +0000

    Deploying to gh-pages from @ StruzhakS/goit-js-hw-11@4f108634fa3a84a6d55cbbcb25902b16aedcd20b 🚀

---
+++ index.a2284a1d.js
@@ -0,0 +1,2 @@
+!function(){var e=new URLSearchParams({"API-key":"36236073-560374128ebdbbd6821cfa057"});document.querySelector(".search-form");fetch("https://pixabay.com/api/",e).then((function(e){return e.json()})).then(console.log)}();
+//# sourceMappingURL=index.a2284a1d.js.map
