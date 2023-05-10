ce225468e55e0b538a5f3e00de228760d2fb17d8
StruzhakS <111219358+StruzhakS@users.noreply.github.com>
2023-05-10 09:57:57 +0000

    Deploying to gh-pages from @ StruzhakS/goit-js-hw-11@0c414a32806aef81adcb136805a364a3247babcd 🚀

--- index.7269525c.js
+++
@@ -1,2 +0,0 @@
-const e=new URLSearchParams({"API-key":"36236073-560374128ebdbbd6821cfa057"});document.querySelector(".search-form");fetch("https://pixabay.com/api/",e).then((e=>e.json())).then(console.log);
-//# sourceMappingURL=index.7269525c.js.map
